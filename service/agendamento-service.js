4c66f08ab44f37fc0d2f1da139e134127f9e2db4
DanielCosta30 <nielcosta24@gmail.com>
2022-12-26 23:28:26 -0300

    feat(aged. js) exibir dados na tela

--- service/agendamento-service.js
+++ service/agendamento-service.js
@@ -1,11 +1,14 @@
-const criaNovaLinha = (nome, email) => {
+const criaNovaLinha = (cliente, pet, data, hora, servico) => {
     const linhaNovoCliente = document.createElement('tr')
     const conteudo = `
-        <td class="td" data-td>${nome}</td>
-                    <td>${email}</td>
+        <td class="td" data-td>${cliente}</td>
+                    <td>${pet}</td>
+                    <td>${data}</td>
+                    <td>${hora}</td>
+                    <td>${servico}</td>
                     <td>
                         <ul class="tabela__botoes-controle">
-                            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                            <li><a href="../telas/edita_agendamento.html" class="botao-simples botao-simples--editar">Editar</a></li>
                             <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
                         </ul>
                     </td> 
@@ -21,15 +24,14 @@ const tabela = document.querySelector('[data-tabela]')
 var listaAgendamento = () => {
     return fetch(`http://localhost:3000/agendamento`)
     .then ( resposta => {
-            return resposta.json()
-       
+            return resposta.json()      
     })
 }
 
 listaAgendamento()
 .then(data => {
-    data.forEach (elmento => {
-        tabela.appendChild (criaNovaLinha ( elemento))
+    data.forEach ( elemento=> {
+        tabela.appendChild (criaNovaLinha ( elemento.cliente, elemento.pet, elemento.data,elemento.hora, elemento.servico ))
     })
 })
 
