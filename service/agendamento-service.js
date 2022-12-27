46422adc2c4a4b67966df5d8934e67f70dff62a2
DanielCosta30 <nielcosta24@gmail.com>
2022-12-27 12:59:24 -0300

    feat(agend.service) isolando comunicação com a API

--- service/agendamento-service.js
+++ service/agendamento-service.js
@@ -1,26 +1,4 @@
-const criaNovaLinha = (cliente, pet, data, hora, servico) => {
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
-        <td class="td" data-td>${cliente}</td>
-                    <td>${pet}</td>
-                    <td>${data}</td>
-                    <td>${hora}</td>
-                    <td>${servico}</td>
-                    <td>
-                        <ul class="tabela__botoes-controle">
-                            <li><a href="../telas/edita_agendamento.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                        </ul>
-                    </td> 
-                    `
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
-}
-
-
-const tabela = document.querySelector('[data-tabela]')
-
-
+// realizar a conexão com a API
 var listaAgendamento = () => {
     return fetch(`http://localhost:3000/agendamento`)
     .then ( resposta => {
@@ -28,12 +6,8 @@ var listaAgendamento = () => {
     })
 }
 
-listaAgendamento()
-.then(data => {
-    data.forEach ( elemento=> {
-        tabela.appendChild (criaNovaLinha ( elemento.cliente, elemento.pet, elemento.data,elemento.hora, elemento.servico ))
-    })
-})
+
+
 
 
 
