a9bfb40a9a5a216c53a197c86bd11e84d5acacc7
DanielCosta30 <nielcosta24@gmail.com>
2022-12-27 13:02:40 -0300

    feat(agend.controller) Cria templade e Exibir dado

---
+++ controller/listaAgedamento-controller.js
@@ -0,0 +1,30 @@
+//cria templade
+const criaNovaLinha = (cliente, pet, data, hora, servico) => {
+    const linhaNovoCliente = document.createElement('tr')
+    const conteudo = `
+        <td class="td" data-td>${cliente}</td>
+                    <td>${pet}</td>
+                    <td>${data}</td>
+                    <td>${hora}</td>
+                    <td>${servico}</td>
+                    <td>
+                        <ul class="tabela__botoes-controle">
+                            <li><a href="../telas/edita_agendamento.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+                        </ul>
+                    </td> 
+                    `
+    linhaNovoCliente.innerHTML = conteudo
+    return linhaNovoCliente
+}
+
+//Percorrer o dom
+const tabela = document.querySelector('[data-tabela]')
+
+// Pegando os dados da API e realizando um loop para exibir os dados na tabela.
+listaAgendamento()
+.then(data => {
+    data.forEach ( elemento=> {
+        tabela.appendChild (criaNovaLinha ( elemento.cliente, elemento.pet, elemento.data,elemento.hora, elemento.servico ))
+    })
+})
