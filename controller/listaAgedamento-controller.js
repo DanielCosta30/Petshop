ee8fe6e61f172cf5c5cdd2b3f707b69cb74df3f7
DanielCosta30 <nielcosta24@gmail.com>
2022-12-27 23:16:09 -0300

    feat(listaagend-controller) import remover

--- controller/listaAgedamento-controller.js
+++ controller/listaAgedamento-controller.js
@@ -1,7 +1,7 @@
 import { agendamentoService } from '../service/agendamento-service.js'
 
 //cria templade
-const criaNovaLinha = (cliente, pet, data, hora, servico) => {
+const criaNovaLinha = (cliente, pet, data, hora, servico, id) => {
     const linhaNovoAgendamento = document.createElement('tr')
     const conteudo = `
         <td class="td" data-td>${cliente}</td>
@@ -17,17 +17,31 @@ const criaNovaLinha = (cliente, pet, data, hora, servico) => {
                     </td> 
                     `
     linhaNovoAgendamento.innerHTML = conteudo
+    linhaNovoAgendamento.dataset.id = id
+    
     return linhaNovoAgendamento
 }
 
 //Percorrer o dom
 const tabela = document.querySelector('[data-tabela]')
 
+tabela.addEventListener('click', (evento) => {
+   var ehbotaoDeletar =  evento.target.className === 'botao-simples botao-simples--excluir'
+   if (ehbotaoDeletar){
+    var linhaAgendamento = evento.target.closest ('[data-id]')
+    var id = linhaAgendamento.dataset.id
+    agendamentoService.removeAgendamento(id)
+    .then(() =>{
+        linhaAgendamento.remove()
+    })
+   }
+})
+
 // Pegando os dados da API e realizando um loop para exibir os dados na tabela.
 
 agendamentoService.listaAgendamento()
 .then(data => {
     data.forEach ( elemento=> {
-        tabela.appendChild (criaNovaLinha ( elemento.cliente, elemento.pet, elemento.data,elemento.hora, elemento.servico ))
+        tabela.appendChild (criaNovaLinha ( elemento.cliente, elemento.pet, elemento.data,elemento.hora, elemento.servico, elemento.id ))
     })
 })
